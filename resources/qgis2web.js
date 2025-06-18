855dbc9f40aabb97722286c7a7872fb5d1333463
Mapping Innovations <200840759+SpynStyr@users.noreply.github.com>
2025-06-18 15:55:02 -0700

    Updated 18June25
    Added June 17 2025 CAR report

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -4,12 +4,12 @@ var map = new ol.Map({
     renderer: 'canvas',
     layers: layersList,
     view: new ol.View({
-        extent: [-13178521.672605, 4023019.180026, -13165728.816195, 4038886.949848], maxZoom: 28, minZoom: 1
+        extent: [-13178898.262316, 4025996.788695, -13165448.172908, 4035914.176073], maxZoom: 28, minZoom: 1
     })
 });
 
 //initial view - epsg:3857 coordinates if not "Match project CRS"
-map.getView().fit([-13178521.672605, 4023019.180026, -13165728.816195, 4038886.949848], map.getSize());
+map.getView().fit([-13178898.262316, 4025996.788695, -13165448.172908, 4035914.176073], map.getSize());
 
 ////small screen definition
     var hasTouchScreen = map.getViewport().classList.contains('ol-touch');

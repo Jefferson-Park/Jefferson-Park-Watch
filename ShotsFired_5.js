8106f4bb29222c6232a96f8fe9175e0141cdafd8
Mapping Innovations <200840759+SpynStyr@users.noreply.github.com>
2026-04-04 15:19:02 -0700

    Claude Update
    removed all files main

---
+++ ShotsFired_5.js
@@ -0,0 +1,9 @@
+var json_ShotsFired_5 = {
+"type": "FeatureCollection",
+"name": "ShotsFired_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "606", "Crime": "SHOTS FIRED", "Address": "27** S Harcourt Ave", "Zip": "90018", "Date": "6/17/2025", "Time": "2255", "Descriptio": "SHOTS FIRED. NO HITS. Unknown suspect shot multiple rounds within city limits at two separate locations.", "latlong": "34.029937659803,-118.347807653576" }, "geometry": { "type": "Point", "coordinates": [ -118.347807653575998, 34.029937659802997 ] } },
+{ "type": "Feature", "properties": { "id": "358", "Crime": "Shots Fired", "Address": "2023 W Jefferson", "Zip": "90018", "Date": "12/7/25", "Time": "10:00:00 PM", "Descriptio": "Unknown suspect(s) fired seven rounds within the city of Los Angeles.", "latlong": "34.025603537479,-118.316007564677" }, "geometry": { "type": "Point", "coordinates": [ -118.315781453565904, 34.025619519998905 ] } }
+]
+}
